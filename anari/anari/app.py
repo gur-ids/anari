1f57ca38fe508179355a53307d521624f97a3dd3
jps <jpsalo@zoho.com>
2019-10-09 09:39:04 +0300

    Removed stats tab

--- anari/anari/app.py
+++ anari/anari/app.py
@@ -18,22 +18,7 @@ teams_df = tm.pre_process('../data/team_stats_2017-2018.csv')
 # handling pre-processed data
 top_players_df = hpf.filter_players_by(df, 'Cap Hit', 4000000)
 
-w_top_df = tm.get_team(df, 'NSH')
-w_bottom_df = tm.get_team(df, 'COL')
-w_out_df = tm.get_team(df, 'STL')
-
-w_top_name = tm.get_team_full_name(teams_df, 'NSH')
-w_bottom_name = tm.get_team_full_name(teams_df, 'COL')
-w_out_name = tm.get_team_full_name(teams_df, 'STL')
-
 max_cap_hit = tm.get_max_cap_hit()
-
-w_bottom_top_paid_df = hpf.top_paid_players(w_bottom_df)
-w_bottom_cap_hit = tm.get_team_total_cap_hit(w_bottom_df)
-w_bottom_top_paid_cap_hit = hpf.cap_hit_share(w_bottom_top_paid_df, w_bottom_cap_hit)
-w_bottom_top_paid_cap_hit_total = hpf.cap_hit_share(w_bottom_top_paid_df, max_cap_hit)
-w_bottom_points = tm.get_team_total_points(w_bottom_df)
-w_bottom_top_paid_points = hpf.points_share(w_bottom_top_paid_df, w_bottom_points)
 
 # produced view
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -43,7 +28,6 @@ app.layout = html.Div([
     html.H1(children='NHL meik mani'),
     dcc.Tabs(id="tabs", value='basic-info-tab', children=[
         dcc.Tab(label='Basic Info', value='basic-info-tab'),
-        dcc.Tab(label='Make statistics great again', value='great-stat-tab'),
         dcc.Tab(label='Team stats', value='team-stats'),
     ]),
     html.Div(id='tabs-content'),
@@ -59,54 +43,6 @@ def render_content(tab):
             g.scatter_plot_toi_pts('toi_pts', top_players_df),
             dcc.Markdown(notes),
         ])
-    elif tab == 'great-stat-tab':
-
-        team_trace_w_top = go.Histogram(
-                x=w_top_df['Cap Hit'],
-        )
-
-        team_trace_out = go.Histogram(
-                x=w_out_df['Cap Hit'],
-        )
-
-        return html.Div([
-            dcc.Markdown(notes),
-            html.H1(children='Western Conference'),
-
-            # TODO: use functions
-            html.H2(children=w_top_name),
-            dcc.Graph(
-                figure={
-                    'data': [team_trace_w_top],
-                    'layout': {
-                        'title': 'Cap Hit distribution',
-                    },
-                }
-            ),
-            # TODO: top players
-
-            html.H2(children=w_bottom_name),
-            g.cap_hit_distribution(w_bottom_df),
-            g.generate_table(w_bottom_top_paid_df),
-            html.Div([
-                html.P(v.top_paid_cap_hit_text(w_bottom_top_paid_cap_hit, w_bottom_cap_hit)),
-                html.P(v.top_paid_max_cap_hit_text(w_bottom_top_paid_cap_hit_total, max_cap_hit)),
-                html.P(v.top_paid_points_text(w_bottom_top_paid_points, w_bottom_points)),
-            ]),
-
-            # TODO: use functions
-            html.H2(children=w_out_name),
-            dcc.Graph(
-                figure={
-                    'data': [team_trace_out],
-                    'layout': {
-                        'title': 'Cap Hit distribution',
-                    },
-                }
-            ),
-            # TODO: top players
-        ])
-
 
 position_filter_data = [
     {'label': 'Center', 'value': 'C'},
